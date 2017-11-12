27790dbac0309b61248bd8fdee6e7cbaa36c090e
H0tan <willy@edv-schacht.de>
2017-11-12 17:40:04 +0100

    saveXML function

--- world_data_parser.php
+++ world_data_parser.php
@@ -6,8 +6,21 @@ class WorldDataParser
          {
          }
 
-         function saveXML()
+         function saveXML($data_array)
          {
+                 $xml_data = new SimpleXMLElement('<?xml version="1.0"?><data></data>');
+                 foreach( $data_array as $key => $value ) {
+                         if( is_numeric($key) ){
+                                 $key = 'item'.$key; //dealing with <0/>..<n/> issues
+                         }
+                         if( is_array($value) ) {
+                                 $subnode = $xml_data->addChild($key);
+                                 array_to_xml($value, $subnode);
+                         } else {
+                                 $xml_data->addChild("$key",htmlspecialchars("$value"));
+                         }
+                 }
+                 $result = $xml_data->asXML('/world_data.xml');
          }
 
          function printXML()
